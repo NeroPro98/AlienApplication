ff1b12c5f4987aab01f6087fcfb3a9d6cfa81c6b
NeroPro98 <www.nero98@gmail.com>
2019-04-10 18:59:41 +0300

    //moazen brunch to work with GUI

---
+++ app/src/main/java/zeon/com/chatapplication/Main_Chats_Page.java
@@ -0,0 +1,13 @@
+package zeon.com.chatapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_Chats_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main__chats__page);
+    }
+}
