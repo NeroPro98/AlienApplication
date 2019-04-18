a71cf9abab41818f80ba9cb9f3b535c6ae8d9b06
NeroPro98 <www.nero98@gmail.com>
2019-04-18 22:49:16 +0300

    copy before new edit

--- app/src/main/java/zeon/com/chatapplication/Search_network.java
+++ app/src/main/java/zeon/com/chatapplication/Search_network.java
@@ -2,12 +2,24 @@ package zeon.com.chatapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class Search_network extends AppCompatActivity {
+
+    ImageView astrodog;
+    ImageButton search;
+    EditText question;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_network);
+
+        astrodog = (ImageView)findViewById(R.id.astrodog);
+        search = (ImageButton)findViewById(R.id.searchnet);
+        question = (EditText)findViewById(R.id.Whatareyoulookingfor);
+
     }
 }
