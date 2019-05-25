547817675a7bd63c2d8d29f6629e3d674c936423
NeroPro98 <www.nero98@gmail.com>
2019-05-25 17:08:35 +0300

    before edit the GUI fot delete and block

--- app/src/main/java/zeon/com/chatapplication/Fragment/Fragment2.java
+++ app/src/main/java/zeon/com/chatapplication/Fragment/Fragment2.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +15,13 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import zeon.com.chatapplication.Adapter.Story_Adapter;
+import zeon.com.chatapplication.Model.UserProfile;
 import zeon.com.chatapplication.Model.story;
+import zeon.com.chatapplication.MyApplication;
 import zeon.com.chatapplication.R;
 
 public class Fragment2 extends Fragment {
@@ -26,12 +30,14 @@ public class Fragment2 extends Fragment {
     ImageView imagestory;
     TextView textadd;
     TextView textdate;
-    ArrayList<story>list = new ArrayList<>();
+    ArrayList<story>listStory = new ArrayList<>();
+    private UserProfile ObjConnection = new UserProfile();
+    ArrayList<Object> list = new ArrayList<Object>();
 
 
     private void InitStory(){
-        list.add(new story(1,"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjR_qew--HhAhWMxoUKHRKwCA0QjRx6BAgBEAU&url=http%3A%2F%2Fsteezo.com%2F%3Fproduct%3Dman-in-stripped-suit&psig=AOvVaw0BK6qUf6tcpUZ1lNMSG0bo&ust=1555962818897341","Mohamad Nserat","March"));
-        list.add(new story(1,"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjB4IbhheLhAhUvxYUKHZESChQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.almasryalyoum.com%2Fnews%2Fdetails%2F998120&psig=AOvVaw0BK6qUf6tcpUZ1lNMSG0bo&ust=1555962818897341","Adnan Ktan","June"));
+        listStory.add(new story(1,"https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwjR_qew--HhAhWMxoUKHRKwCA0QjRx6BAgBEAU&url=http%3A%2F%2Fsteezo.com%2F%3Fproduct%3Dman-in-stripped-suit&psig=AOvVaw0BK6qUf6tcpUZ1lNMSG0bo&ust=1555962818897341","Mohamad Nserat","March"));
+        listStory.add(new story(1,"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjB4IbhheLhAhUvxYUKHZESChQQjRx6BAgBEAU&url=https%3A%2F%2Fwww.almasryalyoum.com%2Fnews%2Fdetails%2F998120&psig=AOvVaw0BK6qUf6tcpUZ1lNMSG0bo&ust=1555962818897341","Adnan Ktan","June"));
     }
 
     @Nullable
@@ -44,7 +50,7 @@ public class Fragment2 extends Fragment {
         textadd = (TextView)view.findViewById(R.id.textadd);
         textdate = (TextView)view.findViewById(R.id.textdate);
         imagestory = (ImageView)view.findViewById(R.id.imagestory);
-
+        Check_All_Story();
        // Picasso.with(getContext()).load("G:\\github\\ChatApplication\\app\\src\\main\\res\\drawable\\astro3.jpg")
              //   .into(imagestory); //don't work
         imagestory.setImageResource(R.drawable.man);
@@ -52,10 +58,78 @@ public class Fragment2 extends Fragment {
         textdate.setText("June");
 
         InitStory();
-        Story_Adapter adapter = new Story_Adapter(getContext(),list);
+        Story_Adapter adapter = new Story_Adapter(getContext(),listStory);
 
         grid.setAdapter(adapter);
 
         return view;
     }
+
+    public boolean Friends_List_Story(ArrayList<Object> arrayList) throws IOException, ClassNotFoundException {
+
+
+        ObjConnection.connectToServer();
+        ObjConnection.SetupStreams();
+        System.out.println("The ArrayList of Fragmint2 is :"+arrayList);
+        ObjConnection.output.writeObject(arrayList);
+        ObjConnection.output.flush();
+        ObjConnection.input.readObject();
+        list = (ArrayList<Object>)ObjConnection.input.readObject();
+        boolean res = ObjConnection.handleReceivedRequest(list);
+        Log.d("res","res:"+res);
+        if(!res){
+
+
+            ObjConnection.CloseCrap();
+            return false;
+
+        }else {
+            ObjConnection.CloseCrap();
+            return true;
+        }
+
+    }
+
+    public ArrayList<Object> serilaizeToStrings(){
+        MyApplication data = (MyApplication)getContext().getApplicationContext();
+        ArrayList<Object>list = new ArrayList<>();
+        list.add(9);
+        list.add(data.getUser_Email());
+        return list;
+    }
+
+    public boolean checkTheListStory() throws InterruptedException, IOException, ClassNotFoundException {
+        boolean res = Friends_List_Story(serilaizeToStrings());
+
+        System.out.println("res of checkTheListStory:"+res);
+        return res;
+    }
+
+    public void Check_All_Story(){
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                boolean bool = false;
+                try {
+
+                    bool = checkTheListStory();
+
+                    System.out.println("The bool of fragment1 is:"+bool);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                if(bool){
+                    InitStory();
+                }
+            }
+        });
+        thread.start();
+
+    }
+
 }
